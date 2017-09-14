670c31bea85fede6985202561429aa6090099c96
Lucas Dohmen <lucas@dohmen.io>
2017-09-14 10:38:46 +0200

    A first spike

--- lib/rredis.rb
+++ lib/rredis.rb
@@ -1,4 +1,109 @@
 require "rredis/version"
 
 module RRedis
+  class Connection
+    attr_reader :engine
+    attr_reader :socket
+
+    def initialize(engine, socket)
+      @engine = engine
+      @socket = socket
+    end
+
+    def self.connect(engine, socket)
+      self.new(engine, socket).handle_connection
+    end
+
+    def handle_connection
+      loop do
+        request_parser = RRedis::Deserializer.new(socket)
+        request = request_parser.deserialize
+
+        break if request.nil?
+
+        response = engine.execute(request)
+
+        response_writer = RRedis::Serializer.new(socket)
+        response_writer.serialize(response)
+      end
+    end
+  end
+
+  class Deserializer
+    ARRAY = /\*(\d+)\r\n/
+    BULK_STRING = /\$(\d+)\r\n/
+
+    def initialize(stream)
+      @stream = stream
+      @request = []
+    end
+
+    def deserialize
+      @request = read
+    end
+
+    private
+
+    def read
+      case @stream.gets
+      when ARRAY then read_array($1.to_i)
+      when BULK_STRING then read_bulk_string($1.to_i)
+      end
+    end
+
+    def read_array(length)
+      length.times.map { read }
+    end
+
+    def read_bulk_string(length)
+      line = @stream.gets
+      line[0...length]
+    end
+  end
+
+  class Engine
+    def initialize
+      @state = {}
+    end
+
+    def execute(command)
+      case command.shift.upcase
+      when "GET" then get(*command)
+      when "SET" then set(*command)
+      end
+    end
+
+    def get(key)
+      @state[key]
+    end
+
+    def set(key, value)
+      @state[key] = value
+      :OK
+    end
+  end
+
+  class Serializer
+    def initialize(output)
+      @output = output
+    end
+
+    def serialize(message)
+      case message
+      when Symbol then write_simple_string(message)
+      when String then write_bulk_string(message)
+      end
+    end
+
+    private
+
+    def write_simple_string(message)
+      @output.write("+#{message}\r\n")
+    end
+
+    def write_bulk_string(message)
+      @output.write("$#{message.length}\r\n")
+      @output.write("#{message}\r\n")
+    end
+  end
 end
--- test/feature_test.rb
+++ test/feature_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
+require "open3"
 
 class FeatureTest < Minitest::Test
-  def test_it_does_something_useful
-    skip "Nothing implemented yet"
+  def setup
+    @stdin, @stdout_and_stderr, @wait_thr = Open3.popen2e("ruby -Ilib exe/rredis")
+    Thread.new do
+      @stdout_and_stderr.each {|l| puts "> #{l}" }
+    end
+    sleep 1
+  end
+
+  def teardown
+    @stdin.close
+    @stdout_and_stderr.close
+    Process.kill("INT", @wait_thr.pid)
+  end
+
+  def test_setting_and_getting
+    assert_response "OK", "set foo bar"
+    assert_response "bar", "get foo"
+  end
+
+  private
+
+  def assert_response(expectation, command)
+    assert_equal expectation, %x[ redis-cli #{command} ].strip
   end
 end
